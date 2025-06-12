8aaf80d40029826880b00b723395c88c653c0dcb
WillPrata <wilson.prata@soulasalle.com.br>
2025-06-12 19:32:04 -0300

    Create Pessoa.java

---
+++ Model/Pessoa.java
@@ -0,0 +1,19 @@
+package model;
+
+public abstract class Pessoa implements Exibivel {
+    protected String nome;
+    protected String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
